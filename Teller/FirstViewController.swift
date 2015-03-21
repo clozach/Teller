569750c58623a6058d5c433f670384fb88d6c009
Chris Lozac'h <clozach@gmail.com>
2015-03-21 13:34:09 -0700

    Initial Commit

---
+++ Teller/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Teller
+//
+//  Created by Chris Lozac'h on 3/21/15.
+//  Copyright (c) 2015 Chris Lozac'h. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Teller/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Teller
+//
+//  Created by Chris Lozac'h on 3/21/15.
+//  Copyright (c) 2015 Chris Lozac'h. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
