01a3f8085d0c13b0bd68dbcf81d0d4df6e5cb329
Chris Lozac'h <chris.lozach@activehours.com>
2015-03-24 11:55:20 -0700

    Added SwiftyJSON pod

--- Podfile
+++ Podfile
@@ -1,8 +1,10 @@
 # Uncomment this line to define a global platform for your project
 platform :ios, '8.0'
+use_frameworks!
 
 target 'Teller' do
 pod 'UIAlertView+Blocks'
+pod "SwiftyJSON", ">= 2.1.3"
 end
 
 target 'TellerTests' do
--- Pods/Target Support Files/Pods-Teller/Pods-Teller-environment.h
+++ Pods/Target Support Files/Pods-Teller/Pods-Teller-environment.h
@@ -6,6 +6,12 @@
 // project.
 
 
+// SwiftyJSON
+#define COCOAPODS_POD_AVAILABLE_SwiftyJSON
+#define COCOAPODS_VERSION_MAJOR_SwiftyJSON 2
+#define COCOAPODS_VERSION_MINOR_SwiftyJSON 1
+#define COCOAPODS_VERSION_PATCH_SwiftyJSON 3
+
 // UIAlertView+Blocks
 #define COCOAPODS_POD_AVAILABLE_UIAlertView_Blocks
 #define COCOAPODS_VERSION_MAJOR_UIAlertView_Blocks 0
